3dfecc1253690f00a8be85694ccceab1270de73f
Gian_Pereira <gian.21.otaku@gmail.com>
2026-04-07 14:33:02 -0300

    begin the vampire

---
+++ kubejs/startup_scripts/vampire.js
@@ -0,0 +1,19 @@
+StartupEvents.registry('item', event => {
+    event.create('blood_bottle')
+        .maxStackSize(16)
+        .useAnimation("drink")
+        .useDuration(itemstack => 20)
+        .use((level, player, hand) => true)
+        .finishUsing((itemstack, level, entity) => {
+            const effects = entity.potionEffects;
+            effects.add('minecraft:regeneration', 5 *20);
+            effects.add('minecraft:saturation', 5)
+            itemstack.shrink(1)
+            if (entity.isPlayer()) {
+                entity.addItem(Item.of('minecraft:glass_bottle'))
+            }
+            return itemstack
+        }).releaseUsing((itemstack, level, entity, tick) => {
+            return itemstack
+        })
+})
