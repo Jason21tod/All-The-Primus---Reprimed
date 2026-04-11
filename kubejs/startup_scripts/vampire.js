1c00bb7da57ad5d6ba32e7af93b8ba4d206f86d3
Gian_Pereira <gian.21.otaku@gmail.com>
2026-04-11 07:42:14 -0300

    did the vampire changes

--- kubejs/startup_scripts/vampire.js
+++ kubejs/startup_scripts/vampire.js
@@ -5,13 +5,16 @@ StartupEvents.registry('item', event => {
         .useDuration(itemstack => 64)
         .use((level, player, hand) => true)
         .finishUsing((itemstack, level, entity) => {
-            const effects = entity.potionEffects;
-            effects.add('minecraft:regeneration', 5 *20);
-            effects.add('minecraft:saturation', 5)
-            itemstack.shrink(1)
             if (entity.isPlayer()) {
-                entity.addItem(Item.of('minecraft:glass_bottle'))
-            }
+                if (entity.tags.contains("vampire")){
+                    const effects = entity.potionEffects;
+                    effects.add('minecraft:regeneration', 5 *20);
+                    effects.add('minecraft:saturation', 5)
+                    itemstack.shrink(1)
+                    entity.addItem(Item.of('minecraft:glass_bottle'))
+                    return itemstack
+            }}
+            entity.tell("Você não pode beber esse líquido...")
             return itemstack
         }).releaseUsing((itemstack, level, entity, tick) => {
             return itemstack
